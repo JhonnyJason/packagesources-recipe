ff29a8d53a96364848e216fd1380a3661516cfec
Lenard Frommelt <lenny09918050@googlemail.com>
2022-04-19 12:09:59 +0200

    Update individualize.js

--- individualize.js
+++ individualize.js
@@ -4,11 +4,10 @@ const fs = require("fs")
 
 const packagePath = pathModule.resolve(process.cwd(), "ressources", "package.json")
 const package = require(packagePath)
-const thingyPath = pathModule.resolve(process.cwd(), "..")
-const name = pathModule.basename(thingyPath)
+const nameWithSources = pathModule.resolve(process.cwd())
+const name = pathModule.basename(thingyPath).replace("-sources", "")
 package.name = name
-package.bin = {}
-package.bin[name] = "./index.js"
+package.type = "module"
 packageString = JSON.stringify(package, null, 2)
 fs.writeFileSync(packagePath, packageString)
 console.log("individualized packagesources!") 
