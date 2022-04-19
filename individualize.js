b2322f40512323070522424e95afe788701fbb11
Lenard Frommelt <lenny09918050@googlemail.com>
2022-04-19 12:10:53 +0200

    Update individualize.js

--- individualize.js
+++ individualize.js
@@ -4,8 +4,8 @@ const fs = require("fs")
 
 const packagePath = pathModule.resolve(process.cwd(), "ressources", "package.json")
 const package = require(packagePath)
-const nameWithSources = pathModule.resolve(process.cwd())
-const name = pathModule.basename(thingyPath).replace("-sources", "")
+const sourcesPath = pathModule.resolve(process.cwd())
+const name = pathModule.basename(sourcesPath).replace("-sources", "")
 package.name = name
 package.type = "module"
 packageString = JSON.stringify(package, null, 2)
